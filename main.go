b786369cde21dca19f1348532d5c25d9bfc370e0
qingbo1011 <61835535+qingbo1011@users.noreply.github.com>
2022-08-08 17:09:50 +0800

    gin route

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"gin-gorm2.0/conf"
+	"gin-gorm2.0/db/mysql"
+	"gin-gorm2.0/route"
+	logging "github.com/sirupsen/logrus"
+)
 
+func main() {
+	r := route.NewRoute()
+	err := r.Run(conf.HttpPort)
+	if err != nil {
+		logging.Fatalln(err)
+	}
+}
+
+func init() {
+	conf.Init("./conf/config.ini")
+	mysql.Init()
 }
---
+++ route/route.go
@@ -0,0 +1,15 @@
+package route
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func NewRoute() *gin.Engine {
+	// gin.Default()和gin.New()的区别：gin.Default()默认使用了2个中间件Logger(), Recovery()
+	r := gin.Default()
+	user := r.Group("/user")
+	{
+		user.POST("/register")
+	}
+	return r
+}
